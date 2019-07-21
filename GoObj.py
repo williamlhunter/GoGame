d5ed74082d12b9a491b8eb476813e4640ca2c182
Bill Hunter <billhunter@Bills-MacBook.local>
2019-07-21 18:29:09 -0500

    worked on next_turn

--- GoObj.py
+++ GoObj.py
@@ -12,10 +12,7 @@ class Group(object):
     
     # merges points from another group into this group.  other group should be subsequently deleted.
     def merge(self, other):
-        if self.color == other.color:
-            self.points += other.points
-            self.calc_liberties()
-            #TODO fix this
+       pass 
     
     #returns all points adjacent to a group
     def get_adjacent_points(self):
@@ -46,6 +43,7 @@ class Game(object):
 
     teams = ('White', 'Black')
     turn_number = 0
+    last_turn_passed = False
     groups = []
 
     cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J',
@@ -54,28 +52,36 @@ class Game(object):
     def __init__(self, size, groups = []):
         self.size = size
         self.board = Board(size, groups)
+        self.cols = self.cols[:size-1]
+        self.rows = range(size)
     
     # performs all actions relevant to a turn.  returns true if the game is over
     def next_turn(self):
         self.turn_number += 1
         color = self.teams[self.turn_number%2]
-        while, True:
+        while True:
             print(f"\n{color}'s turn, where would you like to play?")
-            couplet = input("\n> ")
+            couplet = input("\n> ").upper()
+            if couplet == "PASS"
+                # TODO implement passing
             if len(couplet) is not 2:
-                print('\nPlease specify your move in the form "A9".  Try agian.')
+                print('\nPlease specify your move in the form "A9"')
                 continue
-            if couplet[0] in cols and couplet[1] < self.size and couplet[1] >= 0:
-                print(couplet_to_coords(couplet))
+            if couplet[0] in self.cols and int(couplet[1]) in self.rows:
+                coords = couplet_to_coords(couplet)
                 break
             else:
-                print('\nPlease specify your move in the form "A9".  Try agian.')
+                print('\nOut of Bounds')
+        move = Group(color, coords, self.size)
+        for point in move.get_adjacent_points():
+            pass
+            # TODO implement this
         return False
 
     # returns a 2d tuple containing the coords associated with a couplet ex: "A0" -> (0, 0)
-    def couplet_to_coords(couplet):
-        return (cols.index(couplet[0]), int(couplet[1]))
+    def couplet_to_coords(self, couplet):
+        return (self.cols.index(couplet[0]), int(couplet[1]))
     
     # given a set of coords returns the associated couplet ex: (0, 0)
-    def coords_to_couplet(coords):
+    def coords_to_couplet(self, coords):
         return cols[coords[0]] + str(coords[1])
