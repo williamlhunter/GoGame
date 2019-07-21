28083befb235e2a518ce47a34e51636b403a59c1
Bill Hunter <billhunter@Bills-MacBook.local>
2019-07-21 17:57:54 -0500

    added basic game functionality (choosing board size and incrementing turns

--- GoGame.py
+++ GoGame.py
@@ -1,2 +1,20 @@
-import goobj as go
+import GoObj as go
 
+def main():
+    print('\nWhat size of a board do you want to play on?')
+    print('Available options are "9x9" "13x13" and "19x19":')
+    size_dict = {"9x9": 9, "13x13": 13, "19x19": 19}
+    size = size_dict[input("\n> ")]
+    print(f"Starting a new game on a {size}x{size} board.")
+    game = go.Game(size)
+    while not game.next_turn():
+        pass
+    print(f"\nBlack's score: {game.black_score}")
+    print(f"White's score: {game.white_score}")
+    if game.white_score > game.black_score:
+        winner = "Black"
+    else:
+        winner = "White"
+    print(f"\nCongradulations {winner}!")
+
+main()
--- GoObj.py
+++ GoObj.py
@@ -1,6 +1,12 @@
+"""
+All instances of coords are a touple of zero indexed coordinates in the following order: (column, row)
+"""
+
 class Group(object):
 
-    def __init__(self, color, x, y):
+    liberties = [] # list of coords
+
+    def __init__(self, color, coords, size):
         self.color = color
         self.points = [(x,y)]
     
@@ -15,19 +21,51 @@ class Group(object):
     def get_adjacent_points(self):
         pass
     
+    # sets the liberties of a group to the given tuple of coords 
+    def set_liberties(self, *coords):
+        pass
+
+    
 
 class Board(object):
 
-    groups = []
-
-    def __init__(self, size):
+    def __init__(self, size, groups):
         pass
-    
     
     # places a piece at the given coords
-    def place(self, color, x, y):
+    def place(self, color, coords):
         pass
     
-    def remove(self, color):
+    def remove(self, color, coords):
         pass
     
+class Game(object):
+    
+    black_score = 0
+    white_score = 6.5
+
+    teams = ('White', 'Black')
+    turn_number = 0
+    groups = []
+
+    cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J',
+        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
+
+    def __init__(self, size, groups = []):
+        self.board = Board(size, groups)
+    
+    # performs all actions relevant to a turn.  returns true if the game is over
+    def next_turn(self):
+        self.turn_number += 1
+        color = self.teams[self.turn_number%2]
+        print(f"\n{color}'s turn, where would you like to play?")
+        input("\n> ")
+        return False
+
+    # returns a 2d tuple containing the coords associated with a couplet ex: "A0" -> (0, 0)
+    def couplet_to_coords(couplet):
+        pass
+    
+    # given a set of coords returns the associated couplet ex: (0, 0)
+    def coords_to_couplet(coords):
+        return(cols[coords[0]] + str(coords[1]))
